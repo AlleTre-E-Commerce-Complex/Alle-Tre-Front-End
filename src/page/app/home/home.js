18e6752e806b95b4deb31f828d88061870a13057
Haleem Rafat <haleem.rafat@gmail.com>
2023-03-19 10:56:20 +0200

    add some update

--- src/page/app/home/home.js
+++ src/page/app/home/home.js
@@ -1,5 +1,4 @@
 import React, { useEffect } from "react";
-import CategoriesSlider from "../../../components/home-components/categories-slider";
 import ImageSlider from "../../../components/home-components/image-slider";
 import { SliderData } from "../../../components/home-components/imge-data";
 
@@ -12,9 +11,7 @@ const Home = () => {
       <div className="z-20 ">
         <ImageSlider slides={SliderData} />
       </div>
-      <div className="mt-32">
-        <CategoriesSlider />
-      </div>
+      {/* <div className="bg-gray-100 h-36 "></div> */}
     </div>
   );
 };
