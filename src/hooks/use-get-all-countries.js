299216e00274311f98db1b6baa14e081b5bb2423
kamarudheen mv <kamaru916@gmail.com>
2024-08-21 15:15:19 +0530

    changed the alletre logo in footer and and the url into alletre

--- src/hooks/use-get-all-countries.js
+++ src/hooks/use-get-all-countries.js
@@ -5,27 +5,56 @@ import { useLanguage } from "../context/language-context";
 import useAxios from "./use-axios";
 
 const useGetAllCountries = () => {
+  
   const [lang] = useLanguage();
   const [AllCountriesOptions, setAllCountriesOptions] = React.useState([]);
 
   const { run, isLoading, error, isError } = useAxios();
 
+  // useEffect(() => {
+  //   run(axios.get(api.app.countries.default)).then(({ data }) => {
+  //     console.log('get all contries ===>1',AllCountriesOptions);
+
+      
+  //     const AllCountriesOptions = data.data;
+  //     const options = [];
+
+  //     AllCountriesOptions.forEach((d) =>
+  //       options.push({
+  //         text: lang === "en" ? d?.nameEn : d?.nameAr,
+  //         key: d?.id,
+  //         value: d.id,
+  //       })
+  //     );
+
+  //     setAllCountriesOptions(options);
+  //   })
+  //   .catch(err=>{console.error('Error fetching countries:', err);})
+  // }, [lang, run]);
   useEffect(() => {
-    run(axios.get(api.app.countries.default)).then(({ data }) => {
-      const AllCountriesOptions = data.data;
-      const options = [];
-
-      AllCountriesOptions.forEach((d) =>
-        options.push({
-          text: lang === "en" ? d?.nameEn : d?.nameAr,
-          key: d?.id,
-          value: d.id,
-        })
-      );
-
-      setAllCountriesOptions(options);
-    });
+    console.log('Fetching countries...');
+    run(axios.get(api.app.countries.default))
+      .then(({ data }) => {
+        console.log('API Response:', data);
+        const AllCountriesOptions = data.data;
+        const options = [];
+  
+        AllCountriesOptions.forEach((d) =>
+          options.push({
+            text: lang === "en" ? d?.nameEn : d?.nameAr,
+            key: d?.id,
+            value: d.id,
+          })
+        );
+  
+        setAllCountriesOptions(options);
+      })
+      .catch(err => {
+        console.error('Error fetching countries:', err);
+      });
   }, [lang, run]);
+  
+
 
   return {
     AllCountriesOptions,
--- src/routes.js
+++ src/routes.js
@@ -11,88 +11,88 @@ const routes = {
     },
   },
   app: {
-    default: `/allatre`,
-    home: "/allatre/home",
+    default: `/alletre`,
+    home: "/alletre/home",
     homeDetails: (auctionId = ":auctionId") =>
-      `/allatre/home/${auctionId}/details`,
+      `/alletre/home/${auctionId}/details`,
     payDeposite: (auctionId = ":auctionId") =>
-      `/allatre/home/${auctionId}/details/pay-deposite`,
+      `/alletre/home/${auctionId}/details/pay-deposite`,
     buyNow: (auctionId = ":auctionId") =>
-      `/allatre/home/${auctionId}/details/buy-now`,
+      `/alletre/home/${auctionId}/details/buy-now`,
     paymentSucsess: (auctionId = ":auctionId") =>
-      `/allatre/home/${auctionId}/details/pay-deposite/paymentSucsess`,
+      `/alletre/home/${auctionId}/details/pay-deposite/paymentSucsess`,
     categories: (
       categoryTitle = ":categoryTitle",
       categoryId = ":categoryId"
-    ) => `/allatre/categories/${categoryTitle}/${categoryId}`,
+    ) => `/alletre/categories/${categoryTitle}/${categoryId}`,
 
-    faqs: `/allatre/FAQs`,
-    support: `/allatre/support`,
+    faqs: `/alletre/FAQs`,
+    support: `/alletre/support`,
 
     createAuction: {
-      default: `/allatre/home/create-auction`,
-      productDetails: `/allatre/home/create-auction/product-details`,
-      auctionDetails: `/allatre/home/create-auction/product-details/auction-details`,
-      shippingDetails: `/allatre/home/create-auction/product-details/auction-details/shipping-details`,
-      paymentDetails: `/allatre/home/create-auction/product-details/auction-details/shipping-details/payment-details`,
-      paymentSucsess: `/allatre/home/create-auction/product-details/auction-details/shipping-details/payment-details/Sucsess`,
-      paymentFaild: `/allatre/home/create-auction/product-details/auction-details/shipping-details/payment-details/Faild`,
+      default: `/alletre/home/create-auction`,
+      productDetails: `/alletre/home/create-auction/product-details`,
+      auctionDetails: `/alletre/home/create-auction/product-details/auction-details`,
+      shippingDetails: `/alletre/home/create-auction/product-details/auction-details/shipping-details`,
+      paymentDetails: `/alletre/home/create-auction/product-details/auction-details/shipping-details/payment-details`,
+      paymentSucsess: `/alletre/home/create-auction/product-details/auction-details/shipping-details/payment-details/Sucsess`,
+      paymentFaild: `/alletre/home/create-auction/product-details/auction-details/shipping-details/payment-details/Faild`,
     },
 
     profile: {
-      default: `/allatre/profile`,
-      profileSettings: `/allatre/profile/settings`,
+      default: `/alletre/profile`,
+      profileSettings: `/alletre/profile/settings`,
       myAuctions: {
-        default: `/allatre/profile/my-auctions`,
-        active: `/allatre/profile/my-auctions/active`,
-        scheduled: `/allatre/profile/my-auctions/scheduled`,
-        drafts: `/allatre/profile/my-auctions/drafts`,
-        sold: `/allatre/profile/my-auctions/sold`,
-        pending: `/allatre/profile/my-auctions/pending`,
-        watingForPayment: `/allatre/profile/my-auctions/wating-for-payment`,
-        expired: `/allatre/profile/my-auctions/expired`,
+        default: `/alletre/profile/my-auctions`,
+        active: `/alletre/profile/my-auctions/active`,
+        scheduled: `/alletre/profile/my-auctions/scheduled`,
+        drafts: `/alletre/profile/my-auctions/drafts`,
+        sold: `/alletre/profile/my-auctions/sold`,
+        pending: `/alletre/profile/my-auctions/pending`,
+        watingForPayment: `/alletre/profile/my-auctions/wating-for-payment`,
+        expired: `/alletre/profile/my-auctions/expired`,
 
         activeDetails: (auctionId = ":auctionId") =>
-          `/allatre/profile/my-auctions/active/${auctionId}/details`,
+          `/alletre/profile/my-auctions/active/${auctionId}/details`,
         scheduledDetails: (auctionId = ":auctionId") =>
-          `/allatre/profile/my-auctions/scheduled/${auctionId}/details`,
+          `/alletre/profile/my-auctions/scheduled/${auctionId}/details`,
         soldDetails: (auctionId = ":auctionId") =>
-          `/allatre/profile/my-auctions/sold/${auctionId}/details`,
+          `/alletre/profile/my-auctions/sold/${auctionId}/details`,
         pendingDetails: (auctionId = ":auctionId") =>
-          `/allatre/profile/my-auctions/pending/${auctionId}/details`,
+          `/alletre/profile/my-auctions/pending/${auctionId}/details`,
         watingForPaymentDetails: (auctionId = ":auctionId") =>
-          `/allatre/profile/my-auctions/wating-for-payment/${auctionId}/details`,
+          `/alletre/profile/my-auctions/wating-for-payment/${auctionId}/details`,
         expiredDetails: (auctionId = ":auctionId") =>
-          `/allatre/profile/my-auctions/expired/${auctionId}/details`,
+          `/alletre/profile/my-auctions/expired/${auctionId}/details`,
       },
 
       myBids: {
-        default: `/allatre/profile/my-bids`,
-        inPogress: `/allatre/profile/my-bids/in-pogress`,
-        pending: `/allatre/profile/my-bids/pending`,
-        waitingForDelivery: `/allatre/profile/my-bids/waiting-for-delivery`,
-        expired: `/allatre/profile/my-bids/expired`,
-        completed: `/allatre/profile/my-bids/completed`,
+        default: `/alletre/profile/my-bids`,
+        inPogress: `/alletre/profile/my-bids/in-pogress`,
+        pending: `/alletre/profile/my-bids/pending`,
+        waitingForDelivery: `/alletre/profile/my-bids/waiting-for-delivery`,
+        expired: `/alletre/profile/my-bids/expired`,
+        completed: `/alletre/profile/my-bids/completed`,
 
         inPogressDetails: (auctionId = ":auctionId") =>
-          `/allatre/profile/my-bids/in-pogress/${auctionId}/details`,
+          `/alletre/profile/my-bids/in-pogress/${auctionId}/details`,
         pendingDetails: (auctionId = ":auctionId") =>
-          `/allatre/profile/my-bids/pending/${auctionId}/details`,
+          `/alletre/profile/my-bids/pending/${auctionId}/details`,
         waitingForDeliveryDetails: (auctionId = ":auctionId") =>
-          `/allatre/profile/my-bids/waitingForDelivery/${auctionId}/details`,
+          `/alletre/profile/my-bids/waitingForDelivery/${auctionId}/details`,
         expiredDetails: (auctionId = ":auctionId") =>
-          `/allatre/profile/my-bids/expired/${auctionId}/details`,
+          `/alletre/profile/my-bids/expired/${auctionId}/details`,
         completedDetails: (auctionId = ":auctionId") =>
-          `/allatre/profile/my-bids/completed/${auctionId}/details`,
+          `/alletre/profile/my-bids/completed/${auctionId}/details`,
 
-        completePayment: `/allatre/profile/my-bids/pending/complete-payment`,
+        completePayment: `/alletre/profile/my-bids/pending/complete-payment`,
       },
 
-      watchlist: `/allatre/profile/watchlist`,
+      watchlist: `/alletre/profile/watchlist`,
       watchlistDetails: (auctionId = ":auctionId") =>
-        `/allatre/profile/watchlist/${auctionId}/details`,
+        `/alletre/profile/watchlist/${auctionId}/details`,
 
-      purchased: `/allatre/profile/purchased`,
+      purchased: `/alletre/profile/purchased`,
     },
   },
 };
