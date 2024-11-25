9aedd883a0b837a40d5577f5357b15cad3237f8d
Muhammed Rameez K <muhammedrameez1010@gmail.com>
2024-11-25 17:36:53 +0530

    fix: Image compresssion issue

--- src/component/create-auction-components/add-img-media.js
+++ src/component/create-auction-components/add-img-media.js
@@ -1,287 +1,3 @@
-// import React from "react";
-
-// import { FileUploader } from "react-drag-drop-files";
-
-// import addImage from "../../../src/assets/icons/add-image.svg";
-// import TrashIcon from "../../../src/assets/icons/trash-Icon.png";
-// import { Dimmer, Loader, Segment } from "semantic-ui-react";
-
-// const fileTypes = ["JPEG", "PNG", "GIF", "JPG", "MOV", "mp4"];
-
-// const AddImgMedia = ({
-//   fileOne,
-//   setFileOne,
-//   fileTwo,
-//   setFileTwo,
-//   fileThree,
-//   setFileThree,
-//   fileFour,
-//   setFileFour,
-//   fileFive,
-//   setFileFive,
-// }) => {
-//   const handleChangeOne = (fileOne) => {
-//     setFileOne(fileOne);
-//   };
-//   const handleChangeTwo = (fileTwo) => {
-//     setFileTwo(fileTwo);
-//   };
-//   const handleChangeThree = (fileThree) => {
-//     setFileThree(fileThree);
-//   };
-//   const handleChangeFour = (fileFour) => {
-//     setFileFour(fileFour);
-//   };
-//   const handleChangeFive = (fileFive) => {
-//     setFileFive(fileFive);
-//   };
-
-//   return (
-//     <div className="">
-//       <div className={fileOne && "flex flex-wrap gap-y-4 md:gap-y-0 gap-x-4"}>
-//         <div className={fileTwo && " gap-x-4 flex"}>
-//           <div className="flex flex-wrap gap-y-4 md:gap-y-0 gap-x-4 ">
-//             {fileOne ? (
-//               <div className="relative">
-//                 <div className="sm:w-[299px] w-full h-[189px] hover:bg-gradient-to-t hover:from-[#25252562] absolute z-30 group">
-//                   <div className="flex justify-center mt-36 ">
-//                     <button
-//                       className="w-10 h-10 rounded-full backdrop-blur-md bg-white/50  group-hover:flex  hidden "
-//                       onClick={() => setFileOne(null)}
-//                     >
-//                       <img className="p-3" src={TrashIcon} alt="TrashIcon" />
-//                     </button>
-//                   </div>
-//                 </div>
-//                 <FileUploader handleChange={handleChangeOne} name="file">
-//                   <img
-//                     className="border-primary border-[1px] border-solid rounded-lg w-[299px] h-[189px] object-cover p-[1px]"
-//                     src={URL?.createObjectURL(fileOne)}
-//                     alt="img two"
-//                   />
-//                 </FileUploader>
-//               </div>
-//             ) : (
-//               <div className={fileOne && "hidden"}>
-//                 <div className="cursor-pointer ">
-//                   <FileUploader
-//                     handleChange={handleChangeOne}
-//                     name="file"
-//                     types={fileTypes}
-//                   >
-//                     <div className="border-gray-med border-[1px] border-dashed rounded-lg w-[299px] h-[189px] flex justify-between cursor-pointer ">
-//                       <img
-//                         className="my-auto mx-auto"
-//                         src={addImage}
-//                         alt="imgIcon"
-//                       />
-//                     </div>
-//                   </FileUploader>
-//                 </div>
-//               </div>
-//             )}
-//             {fileTwo ? (
-//               <div className="">
-//                 <div className="relative">
-//                   <div className=" sm:w-[154px] w-full h-[139px]  hover:bg-gradient-to-t hover:from-[#25252562] absolute z-10 group ">
-//                     <div className="flex justify-center mt-24 ">
-//                       <button
-//                         className="w-9 h-9 rounded-full backdrop-blur-md bg-white/50 mt-auto mb-5 group-hover:block hidden "
-//                         onClick={() => setFileTwo(null)}
-//                       >
-//                         <img
-//                           className="p-[9px]"
-//                           src={TrashIcon}
-//                           alt="TrashIcon"
-//                         />
-//                       </button>
-//                     </div>
-//                   </div>
-//                   <FileUploader handleChange={handleChangeTwo} name="file">
-//                     <img
-//                       className=" border-primary border-[1px] border-solid rounded-lg w-[154px] h-[139px] object-cover p-[1px] "
-//                       src={URL?.createObjectURL(fileTwo)}
-//                       alt="img two"
-//                     />
-//                   </FileUploader>
-//                 </div>
-//               </div>
-//             ) : (
-//               <div
-//                 className={`${fileOne ? "" : "hidden"} ${fileTwo && "hidden"}`}
-//               >
-//                 <div className="cursor-pointer">
-//                   <FileUploader
-//                     handleChange={handleChangeTwo}
-//                     name="file"
-//                     types={fileTypes}
-//                   >
-//                     <div className="border-gray-med border-[1px] border-dashed rounded-lg w-[154px] h-[139px] flex justify-between cursor-pointer ">
-//                       <img
-//                         className="my-auto mx-auto"
-//                         src={addImage}
-//                         alt="imgIcon"
-//                       />
-//                     </div>
-//                   </FileUploader>
-//                 </div>
-//               </div>
-//             )}
-//             {fileThree ? (
-//               <div>
-//                 <div className="relative">
-//                   <div className="w-[154px] h-[139px] hover:bg-gradient-to-t hover:from-[#25252562] absolute z-10 group ">
-//                     <div className="flex justify-center mt-24 ">
-//                       <button
-//                         className="w-9 h-9 rounded-full backdrop-blur-md bg-white/50 mt-auto mb-5 group-hover:block hidden "
-//                         onClick={() => setFileThree(null)}
-//                       >
-//                         <img
-//                           className="p-[9px]"
-//                           src={TrashIcon}
-//                           alt="TrashIcon"
-//                         />
-//                       </button>
-//                     </div>
-//                   </div>
-//                   <FileUploader handleChange={handleChangeThree} name="file">
-//                     <img
-//                       className=" border-primary border-[1px] border-solid rounded-lg w-[154px] h-[139px] object-cover p-[1px]"
-//                       src={URL?.createObjectURL(fileThree)}
-//                       alt="img two"
-//                     />
-//                   </FileUploader>
-//                 </div>
-//               </div>
-//             ) : (
-//               <div
-//                 className={`${fileTwo ? "" : "hidden"} ${
-//                   fileThree && "hidden"
-//                 }`}
-//               >
-//                 <div className="cursor-pointer">
-//                   <FileUploader
-//                     className="hidden"
-//                     handleChange={handleChangeThree}
-//                     name="file"
-//                     types={fileTypes}
-//                   >
-//                     <div className="border-gray-med border-[1px] border-dashed rounded-lg w-[154px] h-[139px] flex justify-between cursor-pointer ">
-//                       <img
-//                         className="my-auto mx-auto"
-//                         src={addImage}
-//                         alt="imgIcon"
-//                       />
-//                     </div>
-//                   </FileUploader>
-//                 </div>
-//               </div>
-//             )}
-//             {fileFour ? (
-//               <div>
-//                 <div className="relative">
-//                   <div className="w-[154px] h-[139px]  hover:bg-gradient-to-t hover:from-[#25252562] absolute z-10 group ">
-//                     <div className="flex justify-center mt-24 ">
-//                       <button
-//                         className="w-9 h-9 rounded-full backdrop-blur-md bg-white/50 mt-auto mb-5 group-hover:block hidden "
-//                         onClick={() => setFileFour(null)}
-//                       >
-//                         <img
-//                           className="p-[9px]"
-//                           src={TrashIcon}
-//                           alt="TrashIcon"
-//                         />
-//                       </button>
-//                     </div>
-//                   </div>
-//                   <FileUploader handleChange={handleChangeFour} name="file">
-//                     <img
-//                       className=" border-primary border-[1px] border-solid rounded-lg w-[154px] h-[139px] object-cover p-[1px]"
-//                       src={URL?.createObjectURL(fileFour)}
-//                       alt="img two"
-//                     />
-//                   </FileUploader>
-//                 </div>
-//               </div>
-//             ) : (
-//               <div
-//                 className={`${fileThree ? "" : "hidden"} ${
-//                   fileFour && "hidden"
-//                 }`}
-//               >
-//                 <div className="cursor-pointer">
-//                   <FileUploader
-//                     className="hidden"
-//                     handleChange={handleChangeFour}
-//                     name="file"
-//                     types={fileTypes}
-//                   >
-//                     <div className="border-gray-med border-[1px] border-dashed rounded-lg w-[154px] h-[139px] flex justify-between cursor-pointer ">
-//                       <img
-//                         className="my-auto mx-auto"
-//                         src={addImage}
-//                         alt="imgIcon"
-//                       />
-//                     </div>
-//                   </FileUploader>
-//                 </div>
-//               </div>
-//             )}
-//             {fileFive ? (
-//               <div>
-//                 <div className="relative">
-//                   <div className="w-[154px] h-[139px]  hover:bg-gradient-to-t hover:from-[#25252562] absolute z-10 group ">
-//                     <div className="flex justify-center mt-24 ">
-//                       <button
-//                         className="w-9 h-9 rounded-full backdrop-blur-md bg-white/50 mt-auto mb-5 group-hover:block hidden "
-//                         onClick={() => setFileFive(null)}
-//                       >
-//                         <img
-//                           className="p-[9px]"
-//                           src={TrashIcon}
-//                           alt="TrashIcon"
-//                         />
-//                       </button>
-//                     </div>
-//                   </div>
-//                   <FileUploader handleChange={handleChangeFour} name="file">
-//                     <img
-//                       className=" border-primary border-[1px] border-solid rounded-lg w-[154px] h-[139px] object-cover p-[1px]"
-//                       src={URL?.createObjectURL(fileFive)}
-//                       alt="img two"
-//                     />
-//                   </FileUploader>
-//                 </div>
-//               </div>
-//             ) : (
-//               <div className={fileFour ? "" : "hidden"}>
-//                 <div className="cursor-pointer">
-//                   <FileUploader
-//                     className="hidden"
-//                     handleChange={handleChangeFive}
-//                     name="file"
-//                     types={fileTypes}
-//                   >
-//                     <div className="border-gray-med border-[1px] border-dashed rounded-lg w-[154px] h-[139px] flex justify-between cursor-pointer ">
-//                       <img
-//                         className="my-auto mx-auto"
-//                         src={addImage}
-//                         alt="imgIcon"
-//                       />
-//                     </div>
-//                   </FileUploader>
-//                 </div>
-//               </div>
-//             )}
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default AddImgMedia;
-
 import React from "react";
 import { FileUploader } from "react-drag-drop-files";
 import imageCompression from "browser-image-compression";
@@ -305,22 +21,76 @@ const AddImgMedia = ({
 }) => {
   const compressImage = async (file) => {
     try {
+      // For debugging
+      console.log("Input file:", file.type, file.size / 1024 / 1024, "MB");
+
+      // Convert HEIC/HEIF to JPEG if needed
+      if (file.type.includes("heic") || file.type.includes("heif")) {
+        // You might want to add heic2any library for HEIC support
+        console.warn(
+          "HEIC/HEIF format detected, compression might not work optimally"
+        );
+      }
+
+      // Skip small files
+      if (file.size <= 800 * 1024) {
+        // If less than 800KB
+        console.log("File already small enough, skipping compression");
+        return file;
+      }
+
       const options = {
-        maxSizeMB: 1, // Max size in MB
-        maxWidthOrHeight: 1024, // Max width/height in pixels
-        useWebWorker: true, // Use web worker for faster compression
+        maxSizeMB: 0.8, // Reduced target size
+        maxWidthOrHeight: 1920,
+        initialQuality: 0.7, // Slightly more aggressive compression
+        useWebWorker: true,
+        fileType: "image/jpeg", // Force JPEG output for consistency
+        preserveExif: true,
+        alwaysKeepResolution: true, // Maintain resolution for high-quality photos
+        exifOrientation: true, // Handle image orientation correctly
       };
-      return await imageCompression(file, options);
+
+      let compressedFile = await imageCompression(file, options);
+
+      // If compression wasn't effective, try one more time with more aggressive settings
+      if (compressedFile.size > file.size * 0.9) {
+        // If saved less than 10%
+        console.log(
+          "First compression not effective, trying again with more aggressive settings"
+        );
+        options.maxSizeMB = 0.5;
+        options.initialQuality = 0.6;
+        compressedFile = await imageCompression(file, options);
+      }
+
+      // Create a new File object
+      const finalFile = new File([compressedFile], file.name, {
+        type: "image/jpeg",
+        lastModified: new Date().getTime(),
+      });
+
+      return finalFile;
     } catch (error) {
       console.error("Error compressing image:", error);
-      return file; // Return original file if compression fails
+      return file;
     }
   };
 
   const handleChange = async (file, setFile) => {
     if (file) {
-      const compressedFile = await compressImage(file);
-      setFile(compressedFile);
+      try {
+        const compressedFile = await compressImage(file);
+        console.log("Original size:", file.size / 1024 / 1024, "MB");
+        console.log(
+          "Compressed size:",
+          compressedFile.size / 1024 / 1024,
+          "MB"
+        );
+        setFile(compressedFile);
+      } catch (error) {
+        console.error("Error handling file:", error);
+        setFile(file);
+      }
     }
   };
 
