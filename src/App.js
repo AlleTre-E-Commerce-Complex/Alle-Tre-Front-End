ef1ac9c95bf26083d3e1b6df1494b3d4c9ba4f26
Muhammed Rameez K <muhammedrameez1010@gmail.com>
2025-02-25 10:24:53 +0400

    feat: Google analytics added

--- src/App.js
+++ src/App.js
@@ -10,64 +10,73 @@ import { useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import BlockedModal from "component/shared/UserBlockedModal/BlockedModal";
 import { HelmetProvider } from "react-helmet-async";
+import { initGA, logPageView } from "./utils/analytics";
 
+function App() {
+  const location = useLocation();
 
-function App() { 
-
-  const location = useLocation()
   useEffect(() => {
-    const searchParams = new URLSearchParams(location.search);
-    const unSubscribe = searchParams.get('unSubscribe') === true;
-    
-    console.log('***************123 Query Params:', searchParams.toString());
-    console.log('*************** unSubscribe:', unSubscribe);
+    initGA();
+    logPageView();
+  }, []);
 
-    // if (unSubscribe) {
-    //     setUnSubscribeModal(true);
-    // }
-}, [location.search]);
+  useEffect(() => {
+    logPageView();
+  }, [location.pathname]);
+
+  //   useEffect(() => {
+  //     const searchParams = new URLSearchParams(location.search);
+  //     const unSubscribe = searchParams.get('unSubscribe') === true;
+
+  //     console.log('***************123 Query Params:', searchParams.toString());
+  //     console.log('*************** unSubscribe:', unSubscribe);
+
+  //     // if (unSubscribe) {
+  //     //     setUnSubscribeModal(true);
+  //     // }
+  // }, [location.search]);
   return (
     <HelmetProvider>
-    <div className="App">
-      <Switch>
-        <Route path={routes.app.default} component={AppLayouts} />
+      <div className="App">
+        <Switch>
+          <Route path={routes.app.default} component={AppLayouts} />
 
-        <Route path={routes.auth.default} component={AuthLayouts} />
-        <Route
-          path={routes.auth.forgetpass.default}
-          component={CredentialsuUpdateLayout}
-        />
-        <Redirect to={`${routes.app.home}?page=1&perPage=28`} />
-      </Switch>
+          <Route path={routes.auth.default} component={AuthLayouts} />
+          <Route
+            path={routes.auth.forgetpass.default}
+            component={CredentialsuUpdateLayout}
+          />
+          <Redirect to={`${routes.app.home}?page=1&perPage=28`} />
+        </Switch>
         {/* Blocked Modal */}
         <BlockedModal />
-      <Toaster
-        position="top-right"
-        reverseOrder={true}
-        gutter={8}
-        containerStyle={{}}
-        toastOptions={{
-          iconTheme: {
-            className: "hidden p-0",
-          },
-          loading: {
-            duration: 5000,
-            className:
-              "w-[430px] h-auto border-l-8 border-[#002189] bg-[#F7F9FF] py-3",
-          },
-          success: {
-            duration: 5000,
-            className:
-              "w-[430px] h-auto border-l-8 border-[#45BF55] bg-[#F7F9FF] py-3",
-          },
-          error: {
-            duration: 5000,
-            className:
-              "w-[430px] h-auto border-l-8 border-[#E53737] bg-[#FFFAFA] py-3",
-          },
-        }}
-      />
-    </div>
+        <Toaster
+          position="top-right"
+          reverseOrder={true}
+          gutter={8}
+          containerStyle={{}}
+          toastOptions={{
+            iconTheme: {
+              className: "hidden p-0",
+            },
+            loading: {
+              duration: 5000,
+              className:
+                "w-[430px] h-auto border-l-8 border-[#002189] bg-[#F7F9FF] py-3",
+            },
+            success: {
+              duration: 5000,
+              className:
+                "w-[430px] h-auto border-l-8 border-[#45BF55] bg-[#F7F9FF] py-3",
+            },
+            error: {
+              duration: 5000,
+              className:
+                "w-[430px] h-auto border-l-8 border-[#E53737] bg-[#FFFAFA] py-3",
+            },
+          }}
+        />
+      </div>
     </HelmetProvider>
   );
 }
